38ed56f0fc6c410348b70efb573aeb755a42050e
Jacobvs <jacobvs2@gmail.com>
2021-02-26 18:25:14 -0500

    first stuff

--- src/converter/ElbonianArabicConverter.java
+++ src/converter/ElbonianArabicConverter.java
@@ -4,6 +4,13 @@ package converter;
 import converter.exceptions.MalformedNumberException;
 import converter.exceptions.ValueOutOfBoundsException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * This class implements a converter that takes a string that represents a number in either the
  * Elbonian or Arabic numeral form. This class has methods that will return a value in the chosen form.
@@ -14,6 +21,8 @@ public class ElbonianArabicConverter {
 
     // A string that holds the number (Elbonian or Arabic) you would like to convert
     private final String number;
+
+    private final Map<String, Integer[]> valueMap = new HashMap<>();
 
     /**
      * Constructor for the ElbonianArabic class that takes a string. The string should contain a valid
@@ -29,9 +38,64 @@ public class ElbonianArabicConverter {
      * in the Elbonian number system.
      */
     public ElbonianArabicConverter(String number) throws MalformedNumberException, ValueOutOfBoundsException {
-
+        valueMap.put("M", new Integer[]{1000, 100});
+        valueMap.put("D", new Integer[]{500, 100});
+        valueMap.put("C", new Integer[]{100, 10});
+        valueMap.put("L", new Integer[]{50, 10});
+        valueMap.put("X", new Integer[]{10, 1});
+        valueMap.put("V", new Integer[]{5, 1});
+        valueMap.put("I", new Integer[]{1, 0});
+        // Elbonian Numerals: M, D, C, L, X, V, I
+        // lowercase Numerals: m, d, c, l, x, v
+        // M,C,X,I repeated up to 3x (XX=20)(MMM=3k)
+        // ORDERED largest to smallest then summed [m, d, c, l, x, v, i]
+        // Subtraction rule: lowercase to left = subtraction
+            // M,C,X lower = 1/10, D,L,V = 1/5
+        // D,L,V can only appear once
+        // dD cannot be followed by C
+        // lL cannot be followed by X
+        // vV cannot be followed by I
+        // mM cannot be followed by D,C but cC is ok
+        // cC cannot be followed by L,X but xX is ok
+        // xX cannot be followed by V,I
         // TODO check to see if the number is valid, then set it equal to the string
         this.number = number;
+        try{
+            int i = Integer.parseInt(number);
+            if (i > 4000)
+                throw new ValueOutOfBoundsException("Provided Arabic Number exceeds 4000!");
+            if (i < 1)
+                throw new ValueOutOfBoundsException("Provided Arabic Number is less than 1!");
+            return;
+        } catch (NumberFormatException e){
+            try {
+                Double.parseDouble(number);
+                throw new ValueOutOfBoundsException("Provided Arabic Number needs to be an Integer!");
+            } catch (NumberFormatException ignored){
+            }
+        }
+
+        if(!number.matches("[MDCLXVImdxlxv]+"))
+            throw new MalformedNumberException("Provided string contains invalid characters");
+
+        if(number.chars().filter(c->c=='D').count() > 1 ||
+                number.chars().filter(c->c=='L').count() > 1 ||
+                number.chars().filter(c->c=='V').count() > 1)
+            throw new MalformedNumberException("D, L, & V can only appear once in a string!");
+
+        Pattern pattern = Pattern.compile("M*m*M+|D*d*D+|C*c*C+|L*l*L+|X*x*X+|V*v*V+|I+");
+        Matcher matcher = pattern.matcher(number);
+
+        ArrayList<String> components = new ArrayList<>();
+        while(matcher.find())
+            components.add(matcher.group());
+
+
+
+
+
+
+
     }
 
     /**
--- src/converter/tests/ConverterTests.java
+++ src/converter/tests/ConverterTests.java
@@ -13,25 +13,51 @@ import static org.junit.Assert.assertEquals;
 public class ConverterTests {
 
     @Test
-    public void ElbonianToArabicSampleTest() throws MalformedNumberException, ValueOutOfBoundsException {
-        ElbonianArabicConverter converter = new ElbonianArabicConverter("1");
-        assertEquals(converter.toElbonian(), "I");
+    public void ElbonianToArabicTest() throws MalformedNumberException, ValueOutOfBoundsException {
+        ElbonianArabicConverter converter = new ElbonianArabicConverter("I");
+        assertEquals(1, converter.toArabic());
+
+        ElbonianArabicConverter conv2 = new ElbonianArabicConverter("MMMXX");
+        assertEquals(3020, conv2.toArabic());
+
+        ElbonianArabicConverter conv3 = new ElbonianArabicConverter("MDCLXVI");
+        assertEquals(1666, conv3.toArabic());
+
+        ElbonianArabicConverter conv4 = new ElbonianArabicConverter("mMlLxX");
+        assertEquals(949, conv4.toArabic());
+
+        ElbonianArabicConverter conv5 = new ElbonianArabicConverter("MMdDLXXvV");
+        assertEquals(2474, conv5.toArabic());
     }
 
     @Test
-    public void ArabicToElbonianSampleTest() throws MalformedNumberException, ValueOutOfBoundsException {
-        ElbonianArabicConverter converter = new ElbonianArabicConverter("I");
-        assertEquals(converter.toArabic(), 1);
+    public void ArabicToElbonianTest() throws MalformedNumberException, ValueOutOfBoundsException {
+        ElbonianArabicConverter converter = new ElbonianArabicConverter("1");
+        assertEquals("I", converter.toElbonian());
+
+        ElbonianArabicConverter conv2 = new ElbonianArabicConverter("3020");
+        assertEquals("MMMXX", converter.toElbonian());
+
+        ElbonianArabicConverter conv3 = new ElbonianArabicConverter("1666");
+        assertEquals("MDCLXVI", converter.toElbonian());
+
+        ElbonianArabicConverter conv4 = new ElbonianArabicConverter("949");
+        assertEquals("mMlLxX", converter.toElbonian());
+
+        ElbonianArabicConverter conv5 = new ElbonianArabicConverter("2474");
+        assertEquals("MMdDLXXvV", converter.toElbonian());
     }
 
     @Test(expected = MalformedNumberException.class)
     public void malformedNumberTest() throws MalformedNumberException, ValueOutOfBoundsException {
-        throw new MalformedNumberException("TEST");
+        ElbonianArabicConverter conv = new ElbonianArabicConverter("mMMM");
+        //throw new MalformedNumberException("TEST");
     }
 
     @Test(expected = ValueOutOfBoundsException.class)
     public void valueOutOfBoundsTest() throws MalformedNumberException, ValueOutOfBoundsException {
-        throw new ValueOutOfBoundsException("0");
+        ElbonianArabicConverter conv = new ElbonianArabicConverter("7000");
+        //throw new ValueOutOfBoundsException("0");
     }
 
     // TODO Add more test cases
